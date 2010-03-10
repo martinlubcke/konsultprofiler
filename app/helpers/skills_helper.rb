0cd1eef3cdf86adb758a4024349d9ec628224466
Martin Lübcke <martin.lubcke@gmail.com>
2010-03-10 07:22:23 +0100

    Corrected sorting in group_by_category

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -1,9 +1,13 @@
 module SkillsHelper
   def group_by_category rankings
     rankings.inject({}) do |h,q|
-      name = (q.respond_to?(:skill) ? q.skill : q).category.name 
+      name = ensure_skill(q).category.name 
       h[name] = (h[name] || []) + [q]
       h
-    end.to_a.collect{|s| [s[0], s[1].sort {|a, b| a.name <=> b.name}]}.sort 
+    end.to_a.collect{|s| [s[0], s[1].sort {|a, b| ensure_skill(a).name <=> ensure_skill(b).name}]}.sort 
+  end
+  
+  def ensure_skill skill
+    skill.respond_to?(:skill) ? skill.skill : skill
   end
 end
