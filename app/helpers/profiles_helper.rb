1a83e27bba82afcbf4fdb1bece73ac56df41207a
Martin Lübcke <martin.lubcke@gmail.com>
2010-03-24 11:56:20 +0100

    Made a workaround for RedCloth.to_latex which did not catch the newlines in the documents on Ubuntu

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,7 @@
 module ProfilesHelper
+  def texify text
+    latex = RedCloth.new(text).to_latex
+    latex = latex[0..-3] if latex.end_with?("\n\n")
+    latex.gsub(/\n/, '\\\\\\\\') + "\n\n"
+  end
 end
